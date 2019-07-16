fa50d7296088beaf74b428edc78cdf28c427b4e2
Nguyen Duy Cuong <cuongw.me@gmail.com>
2019-07-16 23:40:24 +0700

    Improve verify

--- src/validate.js
+++ src/validate.js
@@ -1,49 +1,53 @@
-function validateId(content) {
-  const ids = [
-    'code',
-    'national_id',
-    'name',
-    'dob',
-    'gender',
-    'plan_name',
-    'salary',
-    'effective_date',
-    'expiry_date',
-    'premiums'
+export function validateContent(content) {
+  const keys = [
+    {
+      id: 'code',
+      label: 'Code'
+    },
+    {
+      id: 'national_id',
+      label: 'National ID'
+    },
+    {
+      id: 'name',
+      label: 'Name'
+    },
+    {
+      id: 'dob',
+      label: 'Date of Birth'
+    },
+    {
+      id: 'gender',
+      label: 'Gender'
+    },
+    {
+      id: 'plan_name',
+      label: 'Plan'
+    },
+    {
+      id: 'salary',
+      label: 'Current Salary'
+    },
+    {
+      id: 'effective_date',
+      label: 'Effective Date'
+    },
+    {
+      id: 'expiry_date',
+      label: 'Expiry Date'
+    },
+    {
+      id: 'premiums',
+      label: 'Premiums'
+    }
   ];
   const contentFormatter = content.replace(/"/g, "'").replace(/\s/g, '');
   let count = 0;
-  for (const id of ids) {
-    const fullId = `id='${id}'`;
-    if (contentFormatter.indexOf(fullId) !== -1) {
+  for (const key of keys) {
+    const fullId = `id='${key.id}'`;
+    if (contentFormatter.indexOf(fullId) !== -1 && content.indexOf(key.label) !== -1) {
       count += 1;
     }
   }
   return count > 0;
-}
-
-function validateLabel(content) {
-  const labels = [
-    'Code',
-    'National ID',
-    'Name',
-    'Date of Birth',
-    'Gender',
-    'Plan',
-    'Current Salary',
-    'Effective Date',
-    'Expiry Date',
-    'Premiums'
-  ];
-  let count = 0;
-  for (const label of labels) {
-    if (content.indexOf(label) !== -1) {
-      count += 1;
-    }
-  }
-  return count > 0;
-}
-
-export function validateContent(content) {
-  return validateLabel(content) && validateId(content);
 }
